5e639ca3415ec842f5ff0ca8fb78a71d78f0ec46
GreatnessGrace <graceunstoppable7@gmail.com>
2024-12-12 14:10:42 +0530

    Tricky Questions

---
+++ TrickyQuestions/index.js
@@ -0,0 +1,17 @@
+
+// console.log(('b' + 'a' + + 'a' + 'a').toLowerCase())
+// console.log(1 < 2 < 3)
+// console.log(3 > 2 > 1);
+// console.log('5' - 3);
+// console.log('5' + 3);
+// console.log(+true + !false);
+
+// console.log(Boolean('')); // ?
+// console.log(Boolean('0')); // ?
+// console.log(Boolean(' ')); // ?
+// console.log(Boolean(0)); // ?
+// console.log(Boolean(1)); // ?
+console.log([1, 2] + [3, 4]);
+console.log(0.1 + 0.2 == 0.3);
+
+console.log(0.1 + 0.2)
